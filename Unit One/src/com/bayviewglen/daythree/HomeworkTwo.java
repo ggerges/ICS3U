e3574b646a2b130f4df1a74f421ccab90361a491
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-09 13:03:51 -0400

    Hw 2

---
+++ Unit One/src/com/bayviewglen/daythree/HomeworkTwo.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.daythree;
+
+public class HomeworkTwo {
+
+	public static void main(String[] args) {
+		double volume, radius;
+		radius=2;
+		volume= 3.0/4.0*3.14*(radius*radius*radius);
+		System.out.println("The volume of the sphere with the radius of " + radius + " is " + volume + ".");
+
+	}
+
+}
