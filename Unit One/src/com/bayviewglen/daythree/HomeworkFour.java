5f773265cceac6945b901b5cca17d24a9f394eb0
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-10 17:08:59 -0400

    HW 4

---
+++ Unit One/src/com/bayviewglen/daythree/HomeworkFour.java
@@ -0,0 +1,16 @@
+package com.bayviewglen.daythree;
+
+public class HomeworkFour {
+
+	public static void main(String[] args) {
+		double slope, y1, y2, x1,x2;
+		y2 = 8;
+		y1 = 7;
+		x2 = 15;
+		x1 = 9;
+		slope = (y2 - y1) / (x2 - x1);
+		System.out.println("Given the coordinates (9,7), and (15,8), using the equation for slope, slope= " + y2 + " - " + y1 + " / " + x2 + " - " + x1 + " , slope = " + slope);
+
+	}
+
+}
