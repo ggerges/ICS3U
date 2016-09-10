6ab62e03bb8d2e4cc1bd2047d645cbdf937bd348
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-10 16:53:01 -0400

    Hw 3

---
+++ Unit One/src/com/bayviewglen/daythree/HomeworkThree.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.daythree;
+
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+		double a, b, c, x;
+		double y;
+		
+		a = 2.0;
+		b = 4.7;
+		c = 3.2;
+		x = 6.0;
+		
+		y = a* x*x + b*x + c;
+		
+		System.out.println("In the equation: y = " + a + "x^2 + " + b + "x + " + c + ", when x is = to " + x + ", y is = to " + y );
+	}		
+}
