307d97e570c14f7f5c51702ab8f2917eacdf0d15
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-19 13:24:01 -0400

    HW3

---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkThree.java
@@ -0,0 +1,17 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+		int  week, year, day, hour, min;
+		min = 1;
+		hour = min * 60;
+		day = hour * 24;
+		week = day * 7;
+		year = week * 52;
+		
+		System.out.println("There are " + year + " minutes in a year.");
+
+	}
+
+}
