b63668529edc924a7f7635ceae0195eedce57c64
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-19 13:20:09 -0400

    hw

---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkOne.java
@@ -0,0 +1,15 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		int price;
+		double tax, total;
+			price = 985;
+			tax = 1.055;
+			total = 985 * 1.055;
+		System.out.println("The computer at the price of $" + price + " is $" + total + "");	
+
+	}
+
+}
---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkTwo.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkTwo {
+
+	public static void main(String[] args) {
+		double legnth, width,area, perimeter;
+		legnth = 4.5;
+		width = 2.3;
+		
+		area = (legnth * width);
+		perimeter = (legnth * 2) + (width * 2);
+		System.out.println("The area of the rectangle is " + area + ". The Perimeter of the rectangle is "+ perimeter + ".");
+		
+		
+
+	}
+
+}
