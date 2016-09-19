f1754ce32dd1d5080f7b6ba750e52772594a7bf8
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-19 14:01:16 -0400

    HW

---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkEight.java
@@ -0,0 +1,14 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkEight {
+
+	public static void main(String[] args) {
+		double num, square, root;
+		num = 9;
+		square = num * num;
+		root = Math.sqrt(num);
+		System.out.println ("For the number "+ (int)num + ", the square is "+ (int)square +" and the root is " + (int)root);
+
+	}
+
+}
