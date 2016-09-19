b059a6b060a73a251900bb1434552ae393bfb6eb
ggerges <ggerges@ggergessp3.bayviewglen.ca>
2016-09-19 13:57:02 -0400

    HW

---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkSeven.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkSeven {
+
+	public static void main(String[] args) {
+	int fahernheit, celcius;
+	fahernheit = 98;
+	celcius = (fahernheit - 32) * 5/9;
+	System.out.println("At " + fahernheit + " degress Fahernheit, it is " + celcius + " degress.");
+
+	}
+
+}
---
+++ Unit One/src/com/bayviewglen/daysix/HomeworkSix.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.daysix;
+
+public class HomeworkSix {
+
+	public static void main(String[] args) {
+	int momentum, mass, velocity;
+	mass = 10;
+	velocity = 12;
+	momentum = mass * velocity;
+	System.out.println("The momentum of the " + mass + " kilogram bag travelling at " + velocity + " m/s is " + momentum + " m/s."  );
+	}
+
+}
